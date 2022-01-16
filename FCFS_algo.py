8cd0942d180f2870a91d54d1b527e109d5280803
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-16 21:29:25 +0330

    Update FCFS_algo.py

--- FCFS_algo.py
+++ FCFS_algo.py
@@ -16,12 +16,12 @@ class FCFS:
             if len(self.grantt_chart) == 0:
                 self.grantt_chart.append(
                     ProcessGrantInfo(process,
-                                     0,  # first cpu start
-                                     process.cpu_burst_time1,  # io start time
-                                     process.cpu_burst_time1 + process.io_time,  # sec cpu start
-                                     process.cpu_burst_time1,  # first cpu end
-                                     process.cpu_burst_time1 + process.io_time,  # io end time
-                                     process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)  # sec cpu end
+                                     process.arrival_time,  # first cpu start
+                                     process.arrival_time + process.cpu_burst_time1,  # io start time
+                                     process.arrival_time + process.cpu_burst_time1 + process.io_time,  # sec cpu start
+                                     process.arrival_time + process.cpu_burst_time1,  # first cpu end
+                                     process.arrival_time + process.cpu_burst_time1 + process.io_time,  # io end time
+                                     process.arrival_time + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)  # sec cpu end
                 )
             else:
                 prev_process_grantt = self.grantt_chart[len(self.grantt_chart) - 1]
