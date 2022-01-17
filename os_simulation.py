e10deb18333f6d46561920492a3cee1faa3929c5
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-17 10:14:50 +0330

    add SJF into the OS_sim

--- os_simulation.py
+++ os_simulation.py
@@ -4,12 +4,17 @@ sys.path.append(".")
 from Data_Collector import DataCollector
 from FCFS_algo import FCFS
 from Grantt_Analysis import GranttAnalysis
+from SJF_algo import SJF
 
 csv_input_path = 'test/process_input_data.csv'
 
 data_collector = DataCollector(csv_path=csv_input_path)
-fcfs = FCFS(processes=data_collector.getProcesses())
-grantt_chart = fcfs.cpu_process()
-# print(grantt_chart)
-analysis = GranttAnalysis(grantt_chart=fcfs.grantt_chart)
-analysis.pretty_print('fcfs')
+# ------------------- FCFS -------------------
+# fcfs = FCFS(processes=data_collector.getProcesses())
+# grantt_chart = fcfs.cpu_process()
+# ------------------- SJF -------------------
+sjf = SJF(processes=data_collector.getProcesses())
+grantt_chart = sjf.cpu_process()
+
+analysis = GranttAnalysis(grantt_chart=sjf.grantt_chart)
+analysis.pretty_print('sjf')
