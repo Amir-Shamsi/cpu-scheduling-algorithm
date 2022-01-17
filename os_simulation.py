eaf8255638e3fb23c778665fde26acf27639171e
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-17 20:40:21 +0330

    add Round Robin to OS_sim

--- os_simulation.py
+++ os_simulation.py
@@ -5,6 +5,7 @@ from Data_Collector import DataCollector
 from FCFS_algo import FCFS
 from Grantt_Analysis import GranttAnalysis
 from SJF_algo import SJF
+from RR_algo import RoundRobin
 
 csv_input_path = 'test/process_input_data.csv'
 
@@ -12,9 +13,12 @@ data_collector = DataCollector(csv_path=csv_input_path)
 # ------------------- FCFS -------------------
 # fcfs = FCFS(processes=data_collector.getProcesses())
 # grantt_chart = fcfs.cpu_process()
-# ------------------- SJF -------------------
-sjf = SJF(processes=data_collector.getProcesses())
-grantt_chart = sjf.cpu_process()
+# ------------------- SJF --------------------
+# sjf = SJF(processes=data_collector.getProcesses())
+# grantt_chart = sjf.cpu_process()
+# ------------------- RR ---------------------
+rr = RoundRobin(processes=data_collector.getProcesses())
+grantt_chart = rr.cpu_process(time_quantum=5)
 
-analysis = GranttAnalysis(grantt_chart=sjf.grantt_chart)
+analysis = GranttAnalysis(grantt_chart=rr.grantt_chart)
 analysis.pretty_print('sjf')
