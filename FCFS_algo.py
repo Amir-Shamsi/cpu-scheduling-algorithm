a8bbc9b184ef50b5197206b5a8a2df158c0815db
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-18 12:41:43 +0330

    add mode to support one cycle execute

--- FCFS_algo.py
+++ FCFS_algo.py
@@ -7,8 +7,10 @@ from Grantt_Information import ProcessGrantInfo
 class FCFS:
     processes = []
     grantt_chart = []
+    mode = ''
 
-    def __init__(self, processes):
+    def __init__(self, processes, mode=''):
+        self.mode = mode
         self.processes = processes
 
     def cpu_process(self):
@@ -21,7 +23,8 @@ class FCFS:
                                      process.arrival_time + process.cpu_burst_time1 + process.io_time,  # sec cpu start
                                      process.arrival_time + process.cpu_burst_time1,  # first cpu end
                                      process.arrival_time + process.cpu_burst_time1 + process.io_time,  # io end time
-                                     process.arrival_time + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)  # sec cpu end
+                                     process.arrival_time + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)
+                    # sec cpu end
                 )
             else:
                 prev_process_grantt = self.grantt_chart[len(self.grantt_chart) - 1]
@@ -30,19 +33,25 @@ class FCFS:
                         ProcessGrantInfo(process,
                                          prev_process_grantt.get_end_time() + 0,  # first cpu start
                                          prev_process_grantt.get_end_time() + process.cpu_burst_time1,  # io start time
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,  # sec cpu start
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,
+                                         # sec cpu start
                                          prev_process_grantt.get_end_time() + process.cpu_burst_time1,  # first cpu end
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,  # io end time
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)  # sec cpu end
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,
+                                         # io end time
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)
+                        # sec cpu end
                     )
                 else:
                     self.grantt_chart.append(
                         ProcessGrantInfo(process,
                                          process.arrival_time,  # first cpu start
                                          prev_process_grantt.get_end_time() + process.cpu_burst_time1,  # io start time
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,  # sec cpu start
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,
+                                         # sec cpu start
                                          prev_process_grantt.get_end_time() + process.cpu_burst_time1,  # first cpu end
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,  # io end time
-                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)  # sec cpu end
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time,
+                                         # io end time
+                                         prev_process_grantt.get_end_time() + process.cpu_burst_time1 + process.io_time + process.cpu_burst_time2)
+                        # sec cpu end
                     )
         return self.grantt_chart
