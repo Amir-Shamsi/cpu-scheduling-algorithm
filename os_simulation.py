9e19d2286911f14503e1ee913f270ae406a21336
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-01-28 13:39:49 +0330

    Add mode for choosing algorithm inside OS sim

--- os_simulation.py
+++ os_simulation.py
@@ -11,29 +11,44 @@ from RR_algo import RoundRobin
 
 csv_input_path = 'test/process_input_data.csv'
 
-data_collector = DataCollector(csv_path=csv_input_path)
-# ------------------- FCFS -------------------
-# fcfs = FCFS(processes=data_collector.getProcesses())
-# grantt_chart = fcfs.cpu_process()
-# ------------------- SJF --------------------
-# sjf = SJF(processes=data_collector.getProcesses())
-# grantt_chart = sjf.cpu_process()
-# ------- get a deep copy of processes -------
-processes_copy = []
-for process in data_collector.getProcesses().copy():
-    processes_copy.append(Process(process.process_id,
-                                  process.arrival_time,
-                                  process.cpu_burst_time1,
-                                  process.io_time,
-                                  process.cpu_burst_time2))
-# ------------------- RR ---------------------
-
-
-# rr = RoundRobin(processes=data_collector.getProcesses().copy())
-# grantt_chart = rr.cpu_process(time_quantum=5)
-
-mlfq = MLFQ(processes=data_collector.getProcesses().copy())
-grantt_chart = mlfq.cpu_process()
-
-analysis = GranttAnalysis(grantt_chart=mlfq.grantt_chart, processes=processes_copy)
-analysis.pretty_print('RR')
+while True:
+    print('\033[94m' + '--------------------------------------')
+    print('FCFS = 1 | SJF = 2 | RR = 3 | MLFQ = 4')
+    print('--------------------------------------' + '\033[0m')
+    data_collector = []
+    try:
+        mode = int(input('Enter: '))
+    except Exception as error:
+        print('\033[93m' + 'Error: Wrong Input!')
+        exit(-1)
+    data_collector = DataCollector(csv_path=csv_input_path)
+    # ------- get a deep copy of processes -------
+    processes_copy = []
+    result = None
+    for process in data_collector.getProcesses().copy():
+        processes_copy.append(Process(process.process_id,
+                                      process.arrival_time,
+                                      process.cpu_burst_time1,
+                                      process.io_time,
+                                      process.cpu_burst_time2))
+    if mode == 1:
+        # ------------------- FCFS -------------------
+        result = FCFS(processes=data_collector.getProcesses())
+        grantt_chart = result.cpu_process()
+    elif mode == 2:
+        # ------------------- SJF --------------------
+        result = SJF(processes=data_collector.getProcesses())
+        grantt_chart = result.cpu_process()
+    elif mode == 3:
+        # ------------------- RR ---------------------
+        result = RoundRobin(processes=data_collector.getProcesses().copy())
+        grantt_chart = result.cpu_process(time_quantum=1)
+    elif mode == 4:
+        # ------------------ MLFQ --------------------
+        result = MLFQ(processes=data_collector.getProcesses().copy())
+        grantt_chart = result.cpu_process()
+    else:
+        exit(0)
+    if result is not None:
+        analysis = GranttAnalysis(grantt_chart=result.grantt_chart, processes=processes_copy)
+        analysis.pretty_print(result.ClassName)
